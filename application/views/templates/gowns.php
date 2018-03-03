4885313e2cf13fb84e35a8a25bef411225f00d59
Arakolin <aracoleen1701@yahoo.com>
2018-03-03 22:05:46 +0800

    added modal for decors and design page

--- application/views/templates/gowns.php
+++ application/views/templates/gowns.php
@@ -1,3 +1,37 @@
+ <head>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>/public/bower_components/jquery/dist/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+ </head>
+ <style type="text/css">
+   #img {
+    width: 270px;
+    height: 200px;
+   }
+   .upload-drop-zone {
+  height: 200px;
+  border-width: 2px;
+  margin-bottom: 20px;
+}
+
+/* skin.css Style*/
+.upload-drop-zone {
+  color: #ccc;
+  border-style: dashed;
+  border-color: #ccc;
+  line-height: 200px;
+  text-align: center
+}
+.upload-drop-zone.drop {
+  color: #222;
+  border-color: #222;
+}
+ </style>
+ <body>
  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -8,7 +42,41 @@
     </section>
     <!-- Main content -->
     <section class="content container-fluid">
-    <a href="addEvent.php" class="btn btn-block btn-primary btn-lg">Add Gowns</a>
+    <button type="button" class="btn btn-block btn-primary btn-lg" data-toggle="modal" data-target="#addgowns">Add Gowns</button>
+
+    <!-- Modal for add gown -->
+<div id="addgowns" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Upload Gowns</h4>
+      </div>
+      <div class="modal-body">
+        <p>Select files from your computer</p>
+        <form action="" method="post" enctype="multipart/form-data" id="js-upload-form">
+            <div class="form-inline">
+              <div class="form-group">
+                <input type="file" name="files[]" id="js-upload-files" multiple>
+              </div>
+            </div>
+          </form>
+        <!-- drop zone -->
+        <br>
+        <p>Or drag and drop files below</p>
+          <div class="upload-drop-zone" id="drop-zone">
+            Just drag and drop files here
+          </div>
+      </div>
+      <div class="modal-footer">
+        <button type="submit" class="btn btn-sm btn-primary" id="js-upload-submit">Upload files</button>
+      </div>
+    </div>
+
+  </div>
+</div>   
+ <!-- End of modal -->
  
       <div class="container" id="con">
       <div class="row text-center text-lg-left">
@@ -86,14 +154,55 @@
   <!-- Add the sidebar's background. This div must be placed
   immediately after the control sidebar -->
   <div class="control-sidebar-bg"></div>
-</div>
+
 <!-- ./wrapper -->
 
 <!-- REQUIRED JS SCRIPTS -->
 
 <!-- jQuery 3 -->
-<script src="<?php echo base_url();?>/public/bower_components/jquery/dist/jquery.min.js"></script>
+
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo base_url();?>/public/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url();?>/public/dist/js/adminlte.min.js"></script>
+<script type="text/javascript">
+    + function($) {
+    'use strict';
+
+    // UPLOAD CLASS DEFINITION
+    // ======================
+
+    var dropZone = document.getElementById('drop-zone');
+    var uploadForm = document.getElementById('js-upload-form');
+
+    var startUpload = function(files) {
+        console.log(files)
+    }
+
+    uploadForm.addEventListener('submit', function(e) {
+        var uploadFiles = document.getElementById('js-upload-files').files;
+        e.preventDefault()
+
+        startUpload(uploadFiles)
+    })
+
+    dropZone.ondrop = function(e) {
+        e.preventDefault();
+        this.className = 'upload-drop-zone';
+
+        startUpload(e.dataTransfer.files)
+    }
+
+    dropZone.ondragover = function() {
+        this.className = 'upload-drop-zone drop';
+        return false;
+    }
+
+    dropZone.ondragleave = function() {
+        this.className = 'upload-drop-zone';
+        return false;
+    }
+
+}(jQuery);
+  </script>
+  </body>
--- application/views/templates/ongoingEvents.php
+++ application/views/templates/ongoingEvents.php
@@ -16,13 +16,7 @@
     <!-- Main content -->
 
     <section class="content container-fluid">
-      <?php
-        $employeeRole = $this->session->userdata('role');
-        if ($employeeRole === 'handler') {
-          echo'<button class="btn btn-block btn-primary btn-lg" data-toggle="modal" data-target="#add-event">Add Event</button>';
-         }
-      ?>
-      
+      <button class="btn btn-block btn-primary btn-lg" data-toggle="modal" data-target="#add-event">Add Event</button>
 
 
       <!-- Data table of ongoing events -->
@@ -45,12 +39,9 @@
                     <tbody>
                     <?php 
                       if(!empty($events)){
-                      foreach ($events as $event) {
-                      $empID = $event['eventID'];
-                     ?>
-                      
+                      foreach ($events as $event) { ?> 
                         <tr>
-                          <td><?php echo $event['eventName']; ?></td>
+                          <td><?php echo $event['celebrantName']; ?></td>
                           <td><?php echo $event['clientName']; ?></td>
                           <td><?php echo $event['eventType']; ?></td>
                           <td><?php echo $event['packageType']; ?></td>
@@ -117,7 +108,7 @@
         <!-- /.modal -->
 
                 <!-- add event modal -->
-        <div id="add-event" class="modal fade bd-example-modal-lg" role="dialog">
+        <div id="add-event" class="modal fade" role="dialog">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
@@ -125,8 +116,6 @@
                 <h4 class="modal-title">Add Event</h4>
               </div>
               <div class="modal-body">
-                <div class="row">
-                <div class="col-md-6">
                 <form method="post">
                   <div class="row">
                     <div class="col-lg-12">
@@ -137,60 +126,24 @@
                     </div>
                   </div>
                   <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-lg-6">
                       <div class="form-group">
                         <label>Client Name</label>
                         <input type="text" name="client-name" class="form-control">
                       </div>
                     </div>
-                    
-                  </div>
-                  <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-lg-6">
                       <div class="form-group">
                         <label>Contact Number</label>
                         <input type="text" name="contact-number" class="form-control">
                       </div>
                     </div>
-                    
                   </div>
                   <div class="row">
-                    <div class="col-lg-12">
+                    <div class="col-lg-6">
                       <div class="form-group">
                         <label>Celebrant</label>
                         <input type="text" name="celebrant" class="form-control">
-                      </div>
-                    </div>
-                    
-                  </div>
-                  <div class="row">
-                      <div class="col-lg-12">
-                        <div class="form-group">
-                          <label>Event Location</label>
-                          <input type="text" name="event-loc" class="form-control">
-                        </div>
-                      </div>
-                    </div>
-                  <div class="row">
-                    <div class="col-lg-6">
-                      <div class="form-group">
-                        <label>Event Date</label>
-                        <input type="date" name="event-date" class="form-control">
-                      </div>
-                    </div>
-                    <div class="col-lg-6">
-                      <div class="form-group">
-                        <label>Event Time</label>
-                        <input type="time" name="event-time" class="form-control">
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                      <div class="col-lg-6">
-                      <div class="form-group">
-                        <label>Package Availed</label>
-                        <span class="radio"><label><input type="radio" name="event-time" value="full-Package">Full Package</label></span>
-                        <soan class="radio"><label><input type="radio" name="event-time" value="semi-Package">Semi Package</label></soan>
                       </div>
                     </div>
                     <div class="col-lg-6">
@@ -199,17 +152,43 @@
                         <input type="color" name="motiff" class="form-control">
                       </div>
                     </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-3">
+                      <div class="form-group">
+                        <label>Event Date</label>
+                        <input type="date" name="event-date" class="form-control">
+                      </div>
                     </div>
-                  
-                </div>
+                    <div class="col-lg-3">
+                      <div class="form-group">
+                        <label>Event Time</label>
+                        <input type="time" name="event-time" class="form-control">
+                      </div>
+                    </div>
+                    <div class="col-lg-3">
+                      <div class="form-group">
+                        <label>Package Availed</label>
+                        <span class="radio"><label><input type="radio" name="event-time" value="full-Package">Full Package</label></span>
+                        <soan class="radio"><label><input type="radio" name="event-time" value="semi-Package">Semi Package</label></soan>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-12">
+                      <div class="form-group">
+                        <label>Event Location</label>
+                        <input type="text" name="event-loc" class="form-control">
+                      </div>
+                    </div>
+                  </div>
                   
                   <!-- Services -->
-                  <div class="col-lg-6">
-        
+       
                   <div class="box">
                     <div class="box-body">
                       <div class="table table-responsive">
-                        <table id="svc-tbl" class="table table-hover table-bordered table-condensed table-hover text-center">
+                        <table id="svc-tbl" >
                           <h4>Services</h4>
                           <thead>
                             <tr>
@@ -219,35 +198,20 @@
                             </tr>
                           </thead>
                           <tbody>
-                            <?php
-                            echo count($services);
-                            if (!empty($services)) {
-                               foreach ($services as $service) {
-                              
-                            ?>
+                          
+          
                                 <tr>
-                                  <td><form><span class="form-group checkbox"><label><input type="checkbox" value=""></label><?php echo $service['serviceName'] ?></span></form
-                                  ></td>
+                                  <td><form><span class="form-group checkbox"><label><input type="checkbox" value="<?php $row["serviceName"]; ?>"></label></span></form></td>
                                   <td><input class="form-control" type="text" name="" style="border: none;" placeholder="Insert text here"></td>
                                   <td><input class="form-control" type="text" name="" style="border: none;" placeholder="Insert text here"></td>
                                 </tr>
-                            <?php
-                              }
-                             }else {
-                               echo "0 data";
-                             }  
-                             ?>
         
                           </tbody>
                         </table>
                       </div>
                     </div>
-                  </div>
-                  </div>
-        
-                  <!-- end of services table -->
+                  </div> 
                 </form>
-                </div>
                 <div class="modal-footer">
                   <div class="row">
                     <div class="col-lg-2">
@@ -262,7 +226,6 @@
             </div>
           </div>
         </div>
-      </div>
 
     </section>
     <!-- /.content -->
@@ -311,14 +274,3 @@
     $('input:checkbox').prop('checked', false);
   }
 </script>
-<style>
-  @media screen and (min-with: 768px){
-    #add-event .modal-dialog {
-      with:900px;
-    }
-  }
-
-  #add-event .modal-dialog {
-    width:90%;
-  }
-</style>
