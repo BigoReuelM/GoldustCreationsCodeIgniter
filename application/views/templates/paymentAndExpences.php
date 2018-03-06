ff0bd1c9be4c8d2607df102522bb06fe67d9c2d2
popsbear <bigoreuel.rb@gmail.com>
2018-03-06 21:07:37 +0800

    test test

--- application/views/templates/paymentAndExpences.php
+++ application/views/templates/paymentAndExpences.php
@@ -357,8 +357,15 @@ input[type=submit] {
 <script src="<?php echo base_url();?>/public/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo base_url();?>/public/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Slimscroll -->
+<script src="<?php echo base_url();?>/public/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url();?>/public/bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url();?>/public/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url();?>/public/dist/js/demo.js"></script>
+
 <script>
 function date() {
     var x = document.createElement("INPUT");
