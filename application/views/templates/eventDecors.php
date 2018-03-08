86a08a4ab8d8d5f83146854ec83bbd1f007fca46
Arakolin <aracoleen1701@yahoo.com>
2018-03-08 18:00:46 +0800

    Maricris eto na

--- application/views/templates/eventDecors.php
+++ application/views/templates/eventDecors.php
@@ -112,7 +112,29 @@ input[type=submit] {
               <h3 class="box-title">List Of Decors</h3>    
             </div>
             <div class="col-lg-3">
-              <button type="button" class="btn btn-block btn-primary btn-lg">Add New Decors </button>
+              <button type="button" class="btn btn-block btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Add New Decors </button>
+              <!-- Add new decors -->
+              <div class="modal fade" id="myModal" role="dialog">
+      <div class="modal-dialog">
+      
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Modal Header</h4>
+          </div>
+          <div class="modal-body">
+            <p>Some text in the modal.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  
+              <!-- End -->
             </div>
           </div>
         </div>
