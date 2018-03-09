aa574cd720c258c03fc3f3699c56e9917e8411aa
popsbear <bigoreuel.rb@gmail.com>
2018-03-10 00:38:15 +0800

    weird form resubmits after page refresh

--- application/models/Events_model.php
+++ application/models/Events_model.php
@@ -129,12 +129,14 @@
 			return $query->result_array();
 		}
 
-	public function deleteEvntDecor($decId, $eId){
-			$this->db->where('decorID', $decId);
-			$this->db->where('eventID', $eId);
-			$this->db->delete('eventdecors');
+		public function deleteEvntDecor($decId, $eId){
+				$this->db->where('decorID', $decId);
+				$this->db->where('eventID', $eId);
+				$this->db->delete('eventdecors');
 
-		}	public function addEventPayment($cID, $eID, $ceID, $date, $time, $amount){
+			}	
+
+		public function addEventPayment($cID, $eID, $ceID, $date, $time, $amount){
 			$data = array(
 				'clientID' => $cID,
 				'eventID' => $ceID,
