1afbfa294e4df4f3cbe6e3f54e883a74d991abc7
popsbear <bigoreuel.rb@gmail.com>
2018-03-12 15:55:49 +0800

    additional pages

---
+++ application/views/templates/transactionNav.php
@@ -0,0 +1,24 @@
+<div class="content-wrapper">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-centered">
+              <li><a href="<?php echo base_url('transactions/ongoingTransactions') ?>">Ongoing Transactions</a></li>
+              <li><a href="<?php echo base_url('transactions/finishedTransactions') ?>">Finished Transactions</a></li>
+              <li><a href="<?php echo base_url('transactions/canceledTransactions') ?>">Canceled Transactions</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
