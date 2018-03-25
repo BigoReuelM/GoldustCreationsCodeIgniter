21ac0fde64f2f56c368bc052582395fbaa97dce5
popsbear <bigoreuel.rb@gmail.com>
2018-03-25 14:03:54 +0800

    profile password, details, username change

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -12,6 +12,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			$this->load->helper('url');
 			$this->load->model('user_model');
 			$this->load->library('session');
+			$this->load->helper('form');
+			$this->load->library('form_validation');
+
 		}
 		//WILL LOAD THE REGISTRATION VIEW
 		public function index()
@@ -89,6 +92,119 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   			$this->session->sess_destroy();
   			redirect('user/index', 'refresh');
 		}
+
+		public function updateProfile(){
+			$id= $this->session->userdata('employeeID');
+
+			$data = array('success' => false, 'messages' => array());
+
+			$this->form_validation->set_rules('fname', 'First Name', 'trim');
+			$this->form_validation->set_rules('mname', 'Middle Name', 'trim');
+			$this->form_validation->set_rules('lname', 'Last Name', 'trim');
+			$this->form_validation->set_rules('cNum', 'Contact Number', 'trim');
+			$this->form_validation->set_rules('emailAdd', 'Email Address', 'trim|valid_email');
+			$this->form_validation->set_rules('homeAdd', 'Home Address', 'trim');
+			$this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
+
+			if ($this->form_validation->run()) {
+				$fname = $this->input->post('fname');
+				$mname = $this->input->post('mname');
+				$lname = $this->input->post('lname');
+				$cNum = $this->input->post('cNum');
+				$emailAdd = $this->input->post('emailAdd');
+				$homeAdd = $this->input->post('homeAdd');
+
+				if (!empty($fname)) {
+					$this->user_model->updateUserFirstname($id, $fname);
+				}
+				if (!empty($mname)) {
+					$this->user_model->updateUserMiddlename($id, $mname);
+				}
+				if (!empty($lname)) {
+					$this->user_model->updateUserLastname($id, $lname);
+				}
+				if (!empty($cNum)) {
+					$this->user_model->updateUserCnum($id, $cNum);
+				}
+				if (!empty($emailAdd)) {
+					$this->user_model->updateUserEmail($id, $emailAdd);
+				}
+				if (!empty($homeAdd)) {
+					$this->user_model->updateUserHomeadd($id, $homeAdd);
+				}
+
+				$data['success'] = true;
+			}else{
+				foreach ($_POST as $key => $value) {
+					$data['messages'][$key] = form_error($key);
+				}
+			}
+
+			echo json_encode($data);
+
+		}
+
+		public function changeUserPassword(){
+			$id= $this->session->userdata('employeeID');
+
+			$data = array('success' => false, 'messages' => array());
+
+			$this->form_validation->set_rules('oldPassword', 'Current Password', 'trim|required|callback_password_matches['. $id . ']');
+			$this->form_validation->set_rules('newPassword', 'New Password', 'trim|required|min_length[8]');
+			$this->form_validation->set_rules('passConfirmation', 'Password Confirmation', 'trim|required|matches[newPassword]');
+			$this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
+
+			if ($this->form_validation->run()) {
+
+				$newPassword = $this->input->post('newPassword');
+
+				$this->user_model->updateUserPassword($id, $newPassword);
+
+				$data['success'] = true;
+			}else{
+				foreach ($_POST as $key => $value) {
+					$data['messages'][$key] = form_error($key);
+				}
+			}
+			echo json_encode($data);
+		}
+
+		public function changeUserUsername(){
+			$id= $this->session->userdata('employeeID');
+
+			$data = array('success' => false, 'messages' => array());
+
+			$this->form_validation->set_rules('newUsername', 'Username', 'trim|required');
+			$this->form_validation->set_rules('usernameConfirmation', 'Username Confirmation', 'trim|required|matches[newUsername]');
+			$this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
+
+			if ($this->form_validation->run()) {
+
+				$newUsername = $this->input->post('newUsername');
+
+				$this->user_model->updateUserUsername($id, $newUsername);
+
+				$data['success'] = true;
+			}else{
+				foreach ($_POST as $key => $value) {
+					$data['messages'][$key] = form_error($key);
+				}
+			}
+			echo json_encode($data);
+		}
+
+		public function password_matches($pass, $empID){
+			
+			$password = $this->user_model->getPassword($empID);
+
+			if ($password->password !== $pass){
+			    $this->form_validation->set_message('password_matches', 'The password you supplied does not match your existing password. ');
+			    return FALSE;
+			}
+
+			return TRUE;
+		}
+
 	}
 	
 ?>
