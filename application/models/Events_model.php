bca4685b6e1f8d7f60c53f84145f9436af2d9c86
popsbear <bigoreuel.rb@gmail.com>
2018-04-01 13:02:27 +0800

    calendar not working

--- application/models/Events_model.php
+++ application/models/Events_model.php
@@ -783,7 +783,7 @@
 			$query = $this->db->query("
 				SELECT YEAR(eventDate) as year, MONTH(eventDate) as month, DAY(eventDate) as day, eventID, eventName, eventTime 
 				FROM `events`
-				WHERE eventDate is not null and (eventStatus like 'new' or eventStatus like 'on%going');
+				WHERE eventDate is not null and (eventStatus like 'new' or eventStatus like 'on%going') and eventName is not null and eventTime is not null;
 			");
 
 			return $query->result_array();
