52db66a7b0a93018c89abafd5a26158d7bc1eeca
Reuel Bigo <2121855@slu.edu.ph>
2018-04-27 10:04:39 +0800

    decor upload redirect

--- application/controllers/Events.php
+++ application/controllers/Events.php
@@ -1260,6 +1260,13 @@ class Events extends CI_Controller
 			$this->load->view("templates/footer.php");
 		}
 
+		public function setDecorType(){
+			$decorType = $this->input->post('decor_type');
+			$this->session->set_userdata('decorType', $decorType);
+
+			redirect('events/adminDecors');
+		}
+
 		public function adminDecors(){
 			$this->load->helper('directory');
 			$empID = $this->session->userdata('employeeID');
@@ -1279,7 +1286,8 @@ class Events extends CI_Controller
 			// get all folders inside DECOR folder in uploads folder
 			$data['map'] = directory_map('./uploads/decors/', 1);
 			// get contents of the folder similarly named to the current type selected
-			$decorTypeFoldr = html_escape($this->input->post('decor_type'));
+			// $decorTypeFoldr = html_escape($this->input->post('decor_type'));
+			$decorTypeFoldr = $this->session->userdata('decorType');
 			$this->session->set_userdata('currentType', $decorTypeFoldr);
 			$data['type_map'] = directory_map('./uploads/decors/' . $decorTypeFoldr . '/', 1);
 
@@ -1423,7 +1431,7 @@ class Events extends CI_Controller
 				mkdir('./uploads/decors/' . $newEnumVal);
 			}
 			$this->events_model->addDecType($enumVals, $newEnumVal);
-			$this->adminDecorsHome();
+			redirect('events/adminDecorsHome');
 		}
 
 		public function addNewDesType(){
@@ -1435,7 +1443,7 @@ class Events extends CI_Controller
 				mkdir('./uploads/designs/' . $newEnumVal);
 			}
 			$this->events_model->addDesType($enumVals, $newEnumVal);
-			$this->adminDesignsHome();
+			redirect('events/adminDesignsHome');
 		}
 
 		public function addNewEventDecor(){
