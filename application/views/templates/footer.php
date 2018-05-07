1a32adf09ef401dd1bc86dfb651ce4564c06125a
Reuel <2121855@slu.edu.ph>
2018-05-07 15:35:24 +0800

    change change

--- application/views/templates/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- Main Footer -->
-  <footer class="main-footer text-center">
-    <strong>Copyright &copy; 2018 <a href="#">Goldust Creations</a>.</strong> All rights reserved.
-  </footer>
-
- </body>
-</html>
