f6ea2101331c002f07db2807d204065e278a2bdf
Reuel <2121855@slu.edu.ph>
2018-05-07 15:15:08 +0800

    ongoing rental

--- application/views/templates/ongoingRentals.php
+++ application/views/templates/ongoingRentals.php
@@ -13,106 +13,38 @@
 
         <div class="content">
           <div class="row">
-          <div class="col-md-6"> 
-            <div class="box">
-                <div class="box-header">
-                  <h3 class="box-title">Rentals</h3>
+            <div class="col-lg-3">
+              <div class="well">
+                <p>total number of items rented</p>
+                <p>overdue rentals</p>
+                <p>number of clients with rental</p>
+              </div>
+            </div>
+            <div class="col-md-9"> 
+              <div class="box">
+                  <div class="box-header">
+                  </div>
+                <div class="box-body">
+                  <div  class="table table-responsive">
+                    <table id="Rentals" class="table table-bordered table-condensed">
+                      <thead>
+                        <tr>
+                          <th>Client Name</th>
+                          <th>Contact Number</th>
+                          <th>Item Name</th>
+                          <th>Quantity Rented</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody> 
+                           
+                      </tbody>
+                  </table>
                 </div>
-              <div class="box-body">
-                <div  class="table table-responsive">
-                  <table id="Rentals" class="table table-bordered table-condensed">
-                    <thead>
-                      <tr>
-                        <th>Client Name</th>
-                        <th>Contact Number</th>
-                        <th>Service Name</th>
-                        <th>Action</th>
-                      </tr>
-                    </thead>
-                    <tbody> 
-                        
-                          <?php
-                          if (!empty($tdata)) {
-
-                          foreach($tdata as $d) {
-                            $transInfo = $d['transactionID'];
-                          ?>
-                          <tr>
-                          <td><?php echo $d['clientName']; ?></td>
-                          <td><?php echo $d['contactNumber']; ?></td>
-                          <td><?php echo $d['serviceName']; ?></td>
-                          <td>
-                            <form role="form" method="post" action="<?php echo base_url('transactions/setTransactionID'); ?>">
-                                <button class="btn btn-block" id="transactionID" name="transInfo" type="submit" value="<?php echo($transInfo) ?>"> View Info <i class="fa fa-fw fa-info"> </i>
-                                </button>
-                            </form>
-                          </td>
-                          </tr>
-                          <?php
-                          }
-                          }else{
-                            echo "0 result";
-                          } 
-                          ?>
-                         
-                    </tbody>
-                </table>
               </div>
             </div>
           </div>
         </div>
-
-         <div class="col-md-6">
-              <div class="box">
-                <div class="box-header">
-                  <h3 class="box-title">Event Rentals</h3>
-                </div>
-                <div class="box-body">
-                    <div  class="table table-responsive">
-                      <table id ="Events" class="table table-bordered table-condensed">
-                        <thead>
-                          <tr>
-                            <th>Event Name</th>
-                            <th>Client Name</th>
-                            <th>Contact Number</th>
-                            <th>Service Name</th>
-                            <th>Action</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                            <?php 
-                            if(!empty($evredata)){ 
-                              foreach ($evredata as $ed) { 
-                                $eID = $ed['eventID'];
-                                $clientID = $ed['clientID'];
-                                $es = $ed['eventStatus'];
-                                ?> 
-                                <tr>
-                                  <td><?php echo $ed['eventName']; ?></td>
-                                  <td><?php echo $ed['clientName']; ?></td>
-                                  <td><?php echo $ed['contactNumber']; ?></td>
-                                  <td><?php echo $ed['serviceName']; ?></td>
-                                  <td>
-                                    <form role="form" method="post" action="<?php echo base_url('events/setEventID')?>">
-                                      <input name="clientID" value="<?php echo($clientID)?>" hidden>
-                                      <input name="eventStatus" value="<?php echo($es)?>" hidden>
-                                      <button class="btn btn-default btn-block" id="eventID" name="eventInfo" type="submit" value="<?php echo($eID)?>"> View Info <i class="fa fa-fw fa-info"> </i>
-                                      </button>
-                                    </form>
-                                  </td>
-                                </tr>
-                                <?php
-                                  }
-                                  }else{
-                                   echo "0 result";
-                                  } 
-                                ?>
-                        </tbody>
-                    </table>
-                  </div>
-                </div>
-            </div>
-          </div>
       </section>
 
 
